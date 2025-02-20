d34517601204111622433d66b012f3cd64a17f31
Paulo Lacerda <pclacerda@gmail.com>
2025-02-20 16:03:26 -0300

    added measures to data dictionary

--- tools/__init__.py
+++ tools/__init__.py
@@ -7,12 +7,14 @@ from .ragindex.vector_index_retrieval import get_data_points_from_chat_log
 from .database.types import DataSourcesList, TablesList, SchemaInfo, ValidateSQLQueryResult, ExecuteQueryResult
 from .database.datasources import get_all_datasources_info
 from .database.queries import queries_retrieval
+from .database.measures import measures_retrieval
 from .database.tables import tables_retrieval
 from .database.tables import get_all_tables_info
 from .database.tables import get_schema_info
 from .database.querying import execute_dax_query
 from .database.querying import validate_sql_query
 from .database.querying import execute_sql_query
+
 
 # Common Tools
 from .common.datetools import get_today_date
